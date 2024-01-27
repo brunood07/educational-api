e34af8040e2956fad9e8fca799fa120421285440
brunood07 <brunood07@gmail.com>
2024-01-27 12:54:08 -0300

    add cors and started swagger

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import FastifyCors from '@fastify/cors';
+import FastifySwagger from '@fastify/swagger'
+import FastifySwaggerUI from '@fastify/swagger-ui'
 import { env } from './infra/env';
 import { FastifyHttpServer } from './infra/http/http-server/fastify-http-server';
 import { healthCheckRoutes } from './infra/http/routes/health-check-routes';
@@ -5,6 +9,29 @@ import { studentRoutes } from './infra/http/routes/student-routes';
 
 async function main() {
   const httpServer = new FastifyHttpServer()
+  httpServer.register(FastifyCors, {
+    origin: env.CORS,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  })
+  httpServer.register(FastifySwagger)
+  httpServer.register(FastifySwaggerUI, {
+    theme: {
+      title: 'Educational API'
+    },
+    routePrefix: '/swagger-ui',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest: function (request: FastifyRequest, reply: FastifyReply, next: Function) { next() },
+      preHandler: function (request: FastifyRequest, reply: FastifyReply, next: Function) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header: unknown) => header,
+    transformSpecification: (swaggerObject: unknown, request: FastifyRequest, reply: FastifyReply) => { return swaggerObject },
+    transformSpecificationClone: true
+  })
   httpServer.register(studentRoutes)
   httpServer.register(healthCheckRoutes)
   httpServer.listen(env.PORT)
