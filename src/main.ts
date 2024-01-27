261196a13c2a0321a973b8e3eda12ec9563c4632
brunood07 <brunood07@gmail.com>
2024-01-27 13:07:47 -0300

    adjust e2e workflow

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
 import FastifyCors from '@fastify/cors';
 import FastifySwagger from '@fastify/swagger'
-import FastifySwaggerUI from '@fastify/swagger-ui'
 import { env } from './infra/env';
 import { FastifyHttpServer } from './infra/http/http-server/fastify-http-server';
 import { healthCheckRoutes } from './infra/http/routes/health-check-routes';
@@ -14,24 +12,6 @@ async function main() {
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
   httpServer.register(FastifySwagger)
-  httpServer.register(FastifySwaggerUI, {
-    theme: {
-      title: 'Educational API'
-    },
-    routePrefix: '/swagger-ui',
-    uiConfig: {
-      docExpansion: 'full',
-      deepLinking: false
-    },
-    uiHooks: {
-      onRequest: function (request: FastifyRequest, reply: FastifyReply, next: Function) { next() },
-      preHandler: function (request: FastifyRequest, reply: FastifyReply, next: Function) { next() }
-    },
-    staticCSP: true,
-    transformStaticCSP: (header: unknown) => header,
-    transformSpecification: (swaggerObject: unknown, request: FastifyRequest, reply: FastifyReply) => { return swaggerObject },
-    transformSpecificationClone: true
-  })
   httpServer.register(studentRoutes)
   httpServer.register(healthCheckRoutes)
   httpServer.listen(env.PORT)
