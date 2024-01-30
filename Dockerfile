5b1ebfb5deae75e71730852d5924a8bfdc256901
brunood07 <brunood07@gmail.com>
2024-01-29 21:06:09 -0300

    adjusting dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,4 @@ COPY package.json ./
 RUN npm install
 COPY . . 
 
-EXPOSE 3333
-
 CMD ["npm", "run", "start:render"]
--- src/infra/http/routes/student-routes.ts
+++ src/infra/http/routes/student-routes.ts
@@ -4,5 +4,32 @@ import { RegisterStudentController } from "../controllers/register-student-contr
 export async function studentRoutes(app: FastifyInstance) {
   const registerStudent = new RegisterStudentController();
 
-  app.post('/students', registerStudent.handle);
+  app.post('/students', {
+    schema: {
+      description: 'CREATE STUDENT',
+      tags: ['Students'],
+      summary: 'create student endpoint',
+      body: {
+        type: 'object',
+        properties: {
+          first_name: { type: 'string' },
+          last_name: { type: 'string' },
+          email: { type: 'string' },
+          document: { type: 'string' },
+          phone_number: { type: 'string' },
+          password: { type: 'string' },
+        }
+      },
+      response: {
+        201: {
+          description: 'Successful response',
+          type: 'string'
+        },
+        400: {
+          description: 'User already exists exception',
+          type: 'string',
+        }
+      },
+    }
+  }, registerStudent.handle);
 }
